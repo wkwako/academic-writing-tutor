8ecdd511f6dec2ce5e6c58bc3776c384ec2a75dc
William Kwako <wkwakow@gmail.com>
2026-09-12 23:36:20 -0500

    making the site look a little nicer

--- tutor/views.py
+++ tutor/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from .graph import TutorGraph
 
+tutor = TutorGraph(max_tokens=20000)
+
 def tutor_page(request):
     submitted_text = ""
+    topic = ""
+    purpose = ""
     result = {"feedback": ""}
     if request.method == "POST":
         submitted_text = request.POST.get("passage", "")
         print ("=== SUBMITTED PASSAGE ===")
         print (submitted_text)
 
-        tutor = TutorGraph(max_tokens=20000)
+        
         topic = request.POST.get("topic", "")
         purpose = request.POST.get("purpose", "")
         #topic = "Discuss the causes of the French Revolution"
@@ -19,4 +23,8 @@ def tutor_page(request):
         print ("=== FEEDBACK ===")
         print (result["feedback"])
 
-    return render(request, "tutor/index.html", {"submitted_text": submitted_text, "feedback": result["feedback"]})
+    return render(request, "tutor/index.html", {
+        "submitted_text": submitted_text,
+        "feedback": result["feedback"],
+        "topic_text": topic,
+        "purpose_text": purpose})
